0687b3873160ef738073dd40310cf6dc51da5ee4
waterflysic <104680539+waterflysic@users.noreply.github.com>
2025-03-30 10:43:50 +0100

    Add files via upload
    Added new codes to this

--- conditionals.js
+++ conditionals.js
@@ -73,3 +73,56 @@ if (loggedIn)
 else{
     console.log("Your aren't logged in")
 }
+
+var loggedIn = true;
+var emailVerified = false;
+var cardInfo = true
+
+if(loggedIn && emailVerified && cardInfo)
+{
+    console.log ("Allow the user to purchase courses");
+}
+else{
+    console.log ("Don't allow the user to purchase the courses");
+}
+
+//example 5
+
+var byGoogle = true;
+var byEmail = false;
+var byFacebook = true;
+
+if(byGoogle){
+    console.log("byGoogle");
+}
+if(byEmail){
+    console.log("byEmail");
+}
+if(byFacebook){
+    console.log("byFacebook");
+}
+
+//example 6
+var authenticated = false;
+authenticated ? console.log("SIGN OUT Button") : console.log("LOG IN/SIGN UP")
+
+//example 7
+var user = "test prep";
+
+switch (user) {
+    case "admin":
+        console.log(user , "can get full access");
+        break;
+    case "sub admin":
+        console.log(user , "can get full access to create and delete the courses");
+        break;
+    case "test prep":
+        console.log(user, "can get full access to create / delete the test" );
+        break;
+    case "user":
+        console.log(user , "can ger access to consume the contents");
+        break;
+    default:
+        console.log("trials user is asking for access")
+        break;
+}
