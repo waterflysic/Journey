8a5b1f4216de945d343557ed743ea851628f3e3e
waterflysic <104680539+waterflysic@users.noreply.github.com>
2025-03-31 05:36:57 +0100

    Add files via upload

---
+++ function.js
@@ -0,0 +1,19 @@
+//example 1
+
+function myfirstf(name)
+{
+    console.log("yes, your first f is speaking");
+    console.log("yes, your first f speaking", name);
+}
+myfirstf("person 2");
+myfirstf("person1");
+
+//example 2
+
+function myfirstf (name)
+{
+    console.log("yes your first f speaking");
+    console.log("yes, first f speaking", name);
+} 
+myfirstf(); // this calls function
+myfirstf(); // this refers function // check for difference between calling function and referring function
